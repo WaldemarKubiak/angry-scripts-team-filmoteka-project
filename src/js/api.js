703902cf3fed94574dbf3a7f176cb19a3feaf4fe
Guma96 <mateusz.kutarba96@gmail.com>
2023-04-15 11:48:46 +0200

    Create api.js

---
+++ src/js/api.js
@@ -0,0 +1,5 @@
+const API_KEY = '7181a2b710caf29c7153fca4f71ab7d2';
+const MAIN_URL = 'https://api.themoviedb.org/3';
+
+//---Przykładowe zapytanie do api---///
+//---`${MAIN_URL}/movie/550?api_key=${API_KEY}`;---///
