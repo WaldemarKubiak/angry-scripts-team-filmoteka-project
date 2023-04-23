c4014d9ee4dc713ff2c67d906ba6fcf85aea794d
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-04-23 17:35:44 +0200

    tui pagination

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-export { API_KEY, MAIN_URL};
+export { API_KEY, MAIN_URL };
 
 const API_KEY = '7181a2b710caf29c7153fca4f71ab7d2';
 const MAIN_URL = 'https://api.themoviedb.org/3';
--- src/js/common.js
+++
@@ -1,4 +0,0 @@
-export { qs, qsa };
-    
-const qs = qs => document.querySelector(qs);
-const qsa = qsa => document.querySelector(qsa);
