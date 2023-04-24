b8e43aee7e8d5bf63f9643edce3ca2b8662cb460
LukaszKiwala <113800025+LukaszKiwala@users.noreply.github.com>
2023-04-24 17:57:25 +0000

    Deploying to gh-pages from @ WaldemarKubiak/angry-scripts-team-filmoteka-project@302c8ce8d56663324c9af263e465daed47956a18 🚀

---
+++ index.568988f2.js
@@ -0,0 +1,2 @@
+const goUpBtn=document.querySelector(".go-up-btn");function checkHeight(){window.scrollY>720?goUpBtn.style.display="flex":goUpBtn.style.display="none"}window.addEventListener("scroll",checkHeight),goUpBtn.addEventListener("click",(()=>{window.scrollTo({top:0,left:0,behavior:"smooth"}),git}));
+//# sourceMappingURL=index.568988f2.js.map
--- index.fb2dcc94.js
+++
@@ -1,2 +0,0 @@
-const goUpBtn=document.querySelector(".go-up-btn");function checkHeight(){window.scrollY>30?goUpBtn.style.display="flex":goUpBtn.style.display="none"}window.addEventListener("scroll",checkHeight),goUpBtn.addEventListener("click",(()=>{window.scrollTo({top:0,left:0,behavior:"smooth"})}));
-//# sourceMappingURL=index.fb2dcc94.js.map
