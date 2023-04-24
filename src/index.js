dc4039893b834aff099c8657e2971b0d16e05292
kadulko <114679506+kadulko@users.noreply.github.com>
2023-04-24 11:59:45 +0200

    renderMovieCollection -poprawki
    Dodano:
    - wyświetlacie ocen (ukryte na stronie głównej);
    - background-img przy braku postera;
    - ;hoover po najechaniu na kartę (powiększenie + cień)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './js/api';
 import './js/main-section-home';
 import './js/movie-modal';
 import './js/searchByKeyword';
-import './js/pagination'
+import './js/pagination';
 import './js/local-storage';
 import './js/add-to-watch&queue';
--- src/js/main-section-home.js
+++ src/js/main-section-home.js
@@ -4,4 +4,8 @@ import renderMovieCollection from './renderMovieCollection';
 API.getTrending()
   .then(data => data.results)
   .then(renderMovieCollection)
+  .then(() => {
+    const votes = document.querySelectorAll('.movie-data__vote');
+    votes.forEach(vote => vote.classList.add('movie-data__vote--is-hidden'));
+  })
   .catch(error => console.error(error.message));
