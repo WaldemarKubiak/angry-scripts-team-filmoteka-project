cd16eead03d4b08ddf8e7c67c66f7984ca7d0c38
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-04-24 19:33:25 +0200

    poprawki render-from storage
    poprawki kodu aby działało wyświetlanie library

--- src/js/render-from-storage.js
+++ src/js/render-from-storage.js
@@ -1,49 +1,39 @@
 import nothing from '../images/nothing.jpg';
 import renderMovieCollection from './renderMovieCollection';
-import { watched, queue } from './local-storage';
+import { getWatchedLocalStorage, getQueueLocalStorage } from './local-storage';
 import API from './api';
 import './movie-modal';
-
 const refs = {
   watchedRef: document.querySelector('#watched-btn'),
   queueRef: document.querySelector('#queue-btn'),
   movieCollectionDOM: document.querySelector('.movie-collection'),
 };
-
 refs.watchedRef.addEventListener('click', showWatched);
-// refs.queueRef.addEventListener('click', showQueue);
-
+refs.queueRef.addEventListener('click', showQueue);
 function showWatched() {
-  // refs.watchedRef.add('button-active');
-  // refs.queueRef.remove('button-active');
-
-  // if (watched.length === 0) {
-  //   refs.library.innerHTML = `
-  //     <li class="nothing">
-  //       <img src="${nothing}" alt="There's nothing to see here" />
-  //     </li>`;
-  //   return;
-  const watchedArr = [];
-
-  //   console.log(watched.map(id => API.getMovieByID(id).then(watchedArr.push())));
-  //   console.log(watchedArr);
-  //   renderMovieCollection(watched.map(id => API.getMovieByID(id)));
-  // }
-
-  watched.forEach(id =>
-    API.getMovieByID(id)
-      .then(result => watchedArr.push(result))
-      .catch(error => console.log(error.message)),
-  );
-  console.log(watchedArr);
-
-  renderMovieCollection(watchedArr);
-
-  // if (watched.length === 0) {
-  //   refs.movieCollectionDOM.innerHTML = `
-  //   <li class="nothing">
-  //     <img src="${nothing}" alt="There's nothing to see here" />
-  //   </li>`;
-  //   return;
-  // }
+  const watched = getWatchedLocalStorage();
+  if (watched.length === 0) {
+    refs.movieCollectionDOM.innerHTML = `
+      <li class="nothing">
+        <img src="${nothing}" alt="There's nothing to see here" />
+      </li>`;
+    return;
+  }
+  Promise.all(watched.map(id => API.getMovieByID(id)))
+    .then(movies => renderMovieCollection(movies))
+    .catch(error => console.log(error));
 }
+function showQueue() {
+  const queue = getQueueLocalStorage();
+  if (queue.length === 0) {
+    refs.movieCollectionDOM.innerHTML = `
+      <li class="nothing">
+        <img src="${nothing}" alt="There's nothing to see here" />
+      </li>`;
+    return;
+  }
+  Promise.all(queue.map(id => API.getMovieByID(id)))
+    .then(movies => renderMovieCollection(movies))
+    .catch(error => console.log(error));
+}
+export default { showWatched, showQueue };
