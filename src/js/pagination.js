4b19a6226cda6b5e9d4b2a48982238d9b984db5a
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-04-24 18:58:43 +0200

    added pagination trending

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,14 +1,11 @@
 import API from './api';
 import renderMovieCollection from './renderMovieCollection';
 import Pagination from 'tui-pagination';
-// import 'tui-pagination/dist/tui-pagination.css';
 
 export const renderPagination = ({ page, total_pages }) => {
-  // console.log(page, total_pages);
-
   const options = {
     totalItems: total_pages,
-    itemsPerPage: 10,
+    itemsPerPage: 1,
     visiblePages: 5,
     page: page,
     centerAlign: true,
@@ -26,24 +23,23 @@ export const renderPagination = ({ page, total_pages }) => {
         '<span class="pagination-item tui-page-btn tui-is-disabled tui-{{type}}">' +
         '<span class="pagination-btn tui-ico-{{type}}">{{type}}</span>' +
         '</span>',
-      moreButton: '<a href="#" class="pagination-item tui-page-btn tui-{{type}}-is-ellip"></a>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
     },
   };
 
   const pagination = new Pagination('pagination', options);
-
-  // pagination.on('afterMove', function (eventData) {
-  //   alert('The current page is ' + eventData.page);
-  // });
-
   pagination.on('beforeMove', async function (eventData) {
-    console.log('localstorage', eventData.page);
-    let savePageToLocal = localStorage.setItem(`page`, `${eventData.page}`);
-    return (page = eventData.page);
+    API.getTrending(eventData.page)
+      .then(data => {
+        renderMovieCollection(data.results);
+      })
+      .catch(error => console.error(error.message));
   });
 };
 
-console.log('default',localStorage.getItem(`page`));
 API.getTrending(1)
   .then(data => {
     renderMovieCollection(data.results);
@@ -53,14 +49,3 @@ API.getTrending(1)
     });
   })
   .catch(error => console.error(error.message));
-
-const paginationContaier = document.querySelector('#pagination');
-
-paginationContaier.addEventListener('click', () => {
-  console.log('after click container', localStorage.getItem(`page`));
-  API.getTrending(localStorage.getItem(`page`))
-    .then(data => {
-      renderMovieCollection(data.results);
-    })
-    .catch(error => console.error(error.message));
-});
